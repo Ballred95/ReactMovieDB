67b77ccec942fa160079fba66e52a331ca80454e
Ballred95 <blake.allred95@icloud.com>
2020-06-20 22:25:16 -0600

    created multiple files of .js and .scss to start project

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Main extends Component{
+    render(){
+        return(
+            <div className = 'container'>
+                <h1 className = 'title'>React Movie Search</h1>
+            </div>
+        )
+    }
+}
