dd22ecc7cc8130e5d1ec47a5dfb9822ab118af16
Ballred95 <blake.allred95@icloud.com>
2020-06-21 00:03:39 -0600

    aded api key

--- src/components/searchmovie.js
+++ src/components/searchmovie.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function SearchMovie(){
+
+    const searchMovies = (e) => {
+        e.preventDefault()
+        console.log('submitting')
+
+        const url = 'https://api.themoviedb.org/3/movie/550?api_key=c2b1e253ca03e271ca827d89ccf1b64f'
+    }
+
     return(
         <div>
-            <form className = 'form'>
+            <form className = 'form' onSubmit = {searchMovies}>
                 <label className = 'label' htmlFor = 'query'>Movie Name</label>
                 <input className = 'input' type = 'text' name = 'query' placeholder = 'ie jaws'></input>
                 <button type = 'submit'>Search</button>
