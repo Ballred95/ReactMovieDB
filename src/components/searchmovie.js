6d505745b443472b060bacb7d8dd8458ec56d669
Ballred95 <blake.allred95@icloud.com>
2020-06-21 22:26:08 -0600

    delted hardcoded query to test functionality

--- src/components/searchmovie.js
+++ src/components/searchmovie.js
@@ -7,7 +7,7 @@ export default function SearchMovie(){
     const searchMovies = async (e) => {
         e.preventDefault()
         console.log('submitting')
-        const query = 'Jaws'
+        
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=c2b1e253ca03e271ca827d89ccf1b64f&language=en-US&query=${query}&page=1&include_adult=false`;
 
