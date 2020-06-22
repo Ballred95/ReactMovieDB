18cd8bc050b0e7fc1d11351c54a6e6bee06e08b6
Ballred95 <blake.allred95@icloud.com>
2020-06-21 22:12:23 -0600

    added async, await and used fetch call on api

--- src/components/searchmovie.js
+++ src/components/searchmovie.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 export default function SearchMovie(){
 
-    const searchMovies = (e) => {
+    const searchMovies = async (e) => {
         e.preventDefault()
         console.log('submitting')
+        const query = 'Jaws'
 
-        const url = 'https://api.themoviedb.org/3/movie/550?api_key=c2b1e253ca03e271ca827d89ccf1b64f'
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=c2b1e253ca03e271ca827d89ccf1b64f&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try{
+        const res =  await fetch(url);
+        const data =  await res.json();
+        console.log(data);
+        }catch(err){console.log(error)}
     }
 
     return(
