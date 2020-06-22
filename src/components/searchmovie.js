4d03fcc4b2dfe17d7731a69930537b75b642a887
Ballred95 <blake.allred95@icloud.com>
2020-06-21 22:15:31 -0600

    addded initial use state hook

--- src/components/searchmovie.js
+++ src/components/searchmovie.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SearchMovie(){
+
+    const [query, setQuery] = useState('')
 
     const searchMovies = async (e) => {
         e.preventDefault()
